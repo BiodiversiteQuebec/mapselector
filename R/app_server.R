c8d3eced2a8809117ada6a79eb83b5174ea12ef3
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-12-02 15:22:26 -0500

    remove modules in this case modules seem to only complicate life tbh

--- R/app_server.R
+++ R/app_server.R
@@ -6,6 +6,7 @@
 #' @noRd
 app_server <- function( input, output, session ) {
   # Your application server logic 
-  mod_map_select_server("map_select_ui_1")
-  mod_modal_make_server("modal_make_ui_1", region = reactive(input$`map_select_ui_1-map_shape_click`$id))
+  # mod_map_select_server("map_select_ui_1")
+  output$map <- leaflet::renderLeaflet(make_leaflet_map())
+  mod_modal_make_server("modal_make_ui_1", region = reactive(input$map_shape_click$id))
 }
