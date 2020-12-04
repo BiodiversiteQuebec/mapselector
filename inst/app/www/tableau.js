97f5a37926a83aef087c8f8d78e2e9dcbfbf6963
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2020-12-04 16:54:27 -0500

    don't need this setinput value

--- inst/app/www/tableau.js
+++ inst/app/www/tableau.js
@@ -1,5 +1,4 @@
 $( document ).ready(function() {
-Shiny.setInputValue("redrawMap", false);
 function closeOpenNav() {
  var divs=document.getElementById("main-row").children;
   if(divs[0].style.marginLeft == "-22vw"){
@@ -7,13 +6,11 @@ function closeOpenNav() {
       divs[1].style.width = "80vw";
       document.getElementById("map").style.width = "80vw";
       document.getElementById("closebtn").innerHTML="<";
-      Shiny.setInputValue("redrawMap", true);
   }else{
       divs[0].style.marginLeft = "-22vw";
       divs[1].style.width = "96vw";
       document.getElementById("map").style.width = "96vw";
       document.getElementById("closebtn").innerHTML=">";
-      Shiny.setInputValue("redrawMap", true);
   }
 } 
 });
