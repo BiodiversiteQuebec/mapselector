24c270f467f1313d8ce444d692ebee8e6b08e018
Andrew MacDonald <a.a.m.macdonald@gmail.com>
2021-06-16 17:25:19 -0400

    correct the campaign type module

--- R/mod_map_campaign_type.R
+++ R/mod_map_campaign_type.R
@@ -91,35 +91,3 @@ mod_campaign_type_server <- function(id){
 ## To be copied in the server
 # mod_map_campaign_type_server("map_campaign_type_ui_1")
 
-# test function
-testapp_map_campaign_type <- function(){
-  ui <- fluidPage(
-    fa_dependency(),
-    mod_campaign_type_checkbox("ff"),
-    textOutput("what"),
-    textOutput("where"),
-    tableOutput("how_many"),
-    mod_campaign_type_map_plot("ff")
-  )
-  
-  server <-  function(input, output, session) {
-    
-    out <- mod_campaign_type_server("ff")
-    
-    outtext <- reactive(paste("you just selected", paste(out$camps(), collapse = " ")))
-    outclik <- reactive(paste("you just clicked the site", out$click()))
-    outobvs <- reactive({
-      req(out$click)
-      out$stdat()[c("obs_species.taxa_name", 
-                                      "obs_species.value")]
-      })
-    output$what <- renderText(outtext())
-    output$where <- renderText(outclik())
-    output$how_many <- renderTable(outobvs())
-  }
-  shinyApp(ui, server)
-}
-
-
-
-# reactlog::reactlog_enable()
---
+++ tests/testthat/test-mod_map_campaign_type.R
@@ -0,0 +1,40 @@
+test_that("mod_campaign_type module works", {
+  skip_if_not(interactive())
+  
+  # test function
+  testapp_map_campaign_type <- function(){
+    ui <- shiny::fluidPage(
+      fa_dependency(),
+      mod_campaign_type_checkbox("ff"),
+      shiny::textOutput("what"),
+      shiny::textOutput("where"),
+      shiny::tableOutput("how_many"),
+      mod_campaign_type_map_plot("ff")
+    )
+    
+    server <-  function(input, output, session) {
+      
+      out <- mod_campaign_type_server("ff")
+      
+      outtext <- reactive(paste("you just selected", paste(out$camps(), collapse = " ")))
+      outclik <- reactive(paste("you just clicked the site", out$click()))
+      outobvs <- reactive({
+        req(out$click)
+        out$stdat()[c("obs_species.taxa_name", 
+                      "obs_species.value")]
+      })
+      output$what <- renderText(outtext())
+      output$where <- renderText(outclik())
+      output$how_many <- renderTable(outobvs())
+    }
+    shiny::shinyApp(ui, server)
+  }
+  
+  
+  
+  # reactlog::reactlog_enable()
+  
+  testapp_map_campaign_type()
+  
+  
+})
